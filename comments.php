fafa8bce466864ffa8b22526c2ea6e51e3df4c9d
JhonellaJazmine <68491417+JhonellaJazmine@users.noreply.github.com>
2021-10-27 03:48:44 +0800

    Add files via upload

---
+++ comments.php
@@ -0,0 +1,21 @@
+
+
+<?php 
+
+//there are two kind of comments
+
+#this line is commented
+
+// this line is also commented
+
+/*
+
+this is a 
+multi-line 
+comment
+
+*/
+
+echo "no comments";
+
+?>
