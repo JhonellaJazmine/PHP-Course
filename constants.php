3bcd83666ec077524eb3cb39712561967cf3462a
JhonellaJazmine <68491417+JhonellaJazmine@users.noreply.github.com>
2021-10-27 03:34:37 +0800

    Add files via upload

---
+++ constants.php
@@ -0,0 +1,11 @@
+
+
+<?php
+//constants value cannot be changed once it's define it cannot be changed
+
+ define("CONSTANT", "this is a CONSTANT", false);
+ define("NUMBER", 99);
+
+ echo NUMBER;
+ echo "<br>";
+ echo CONSTANT;
