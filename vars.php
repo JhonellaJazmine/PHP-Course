1e36c5cf9aaf5eacf40caf4cd6b76ce1ab9a7bf4
JhonellaJazmine <68491417+JhonellaJazmine@users.noreply.github.com>
2021-10-27 03:23:16 +0800

    Add files via upload

---
+++ vars.php
@@ -0,0 +1,23 @@
+
+
+<?php
+ 
+  // a variable is a data container
+
+  // put $ first to declare variable. you cannot use any number or sign after the $ execpt for _
+
+  $username = "jhonella123";
+
+  //variables are case sensitive
+
+  $NUM = 9;
+  $num = 8;
+  echo $username;
+
+  echo "<br>";//next line space
+
+  echo $num;
+  echo "<br>";//next line space
+  echo $NUM;
+
+  // variable $num is different from $NUM
