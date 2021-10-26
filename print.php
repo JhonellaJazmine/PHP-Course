96b26052a64c4b93e6c1ea8b8fa510b1b5de63cf
JhonellaJazmine <68491417+JhonellaJazmine@users.noreply.github.com>
2021-10-27 03:16:07 +0800

    Add files via upload

---
+++ print.php
@@ -0,0 +1,21 @@
+
+
+<?php
+
+print "hello from print";
+
+print 9-7;
+
+print '<br>';
+
+// can't declare a var on echo
+
+// $var = print("hello from var");
+
+// $var_2 = echo "hello from var_2";
+
+// echo is faster than print
+// print can only hold 1 argument
+
+echo "hello this is argument one " , "hello from argument 2";
+
