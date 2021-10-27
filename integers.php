ca12f1137ecfe83c5e5dd29c72d681e95f4774a5
JhonellaJazmine <68491417+JhonellaJazmine@users.noreply.github.com>
2021-10-27 21:03:04 +0800

    Add files via upload

---
+++ integers.php
@@ -0,0 +1,16 @@
+
+
+<?php
+ 
+ /* integers  - numbers can be positive or negative
+ floats/dec
+ booleans
+ strings
+ null
+ */
+
+ $num = 4;
+
+var_dump($num); // displagets and display the datatype with the value of the variable
+
+echo gettype($num);  // gets and display the datatype
