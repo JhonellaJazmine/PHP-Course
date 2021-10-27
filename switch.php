77535bdae01382992b429eb11591d9d9185a2a6a
JhonellaJazmine <68491417+JhonellaJazmine@users.noreply.github.com>
2021-10-28 00:19:17 +0800

    Add files via upload

---
+++ switch.php
@@ -0,0 +1,21 @@
+
+
+<?php
+
+ //switch statement
+
+ $favColor = "black";
+
+ switch ($favColor){
+     case "red":
+        echo "your favorite color is red";
+        break;
+
+     case "green":
+        echo "your favorite color is green";
+        break;
+
+        default:
+           echo "you like another color";
+ }
+ 
