2a13aa3233140712f5bcfdc491d483e8269f821b
JhonellaJazmine <68491417+JhonellaJazmine@users.noreply.github.com>
2021-10-27 23:00:49 +0800

    Add files via upload

---
+++ operators.php
@@ -0,0 +1,23 @@
+
+
+<?php
+ 
+ /*
+    +
+    -
+    *
+    /
+    %
+ */
+
+
+ echo 8 + 8.9;
+ echo "<br>";
+ echo 12 - 11;
+ echo "<br>";
+ echo 22 * 1;
+ echo "<br>";
+ echo 11 / 3;
+ echo "<br>";
+ echo 9 % 3;
+
