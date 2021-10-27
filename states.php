26c500135c7d43179e3867287fcff6c502b0778b
JhonellaJazmine <68491417+JhonellaJazmine@users.noreply.github.com>
2021-10-28 00:12:39 +0800

    Add files via upload

---
+++ states.php
@@ -0,0 +1,21 @@
+
+
+<?php
+
+ //if elseif else statement
+
+ //switch
+
+ $age = 18;
+
+ if ($age == 12)
+ {
+     //action
+    echo "you should not be here";
+ } elseif ($age== 18) 
+ {
+    echo "you are 18. you are welcome";
+ } else
+ {
+    echo "you are welcome here";
+ }
