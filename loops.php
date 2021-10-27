24e0ff4e5e7e3b096dbbe5960e5adb37ef3cb7d9
JhonellaJazmine <68491417+JhonellaJazmine@users.noreply.github.com>
2021-10-28 01:25:46 +0800

    Add files via upload

---
+++ loops.php
@@ -0,0 +1,28 @@
+
+
+<?php
+
+//  //while and the for loops
+//     //the initialization
+
+
+//     $i = 0;
+//  while ($i <= 10){
+
+    
+//     //the increment or the decrement
+
+//     echo $i++;
+//     echo "<br>";
+//  }
+
+//for loop
+
+
+$i = 20;
+
+for($i = 0; $i <= 20; $i++)
+{
+    echo "value is: $i";
+    echo "<br>";
+}
